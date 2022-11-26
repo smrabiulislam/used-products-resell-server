dab45d55116fe43bd2db5450a678ebf4788f17b3
smrabiulislam <smarbiulislam03@gmail.com>
2022-11-27 02:27:09 +0600

    product category added

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dkuqjjn.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        const categoriesCollection = client
+            .db("resellerHub")
+            .collection("categories");
+
+        const productsCollection = client
+            .db("resellerHub")
+            .collection("products");
+
+        const usersCollection = client
+            .db("resellerHub")
+            .collection("users");
+
+        const bookingsCollection = client
+            .db("resellerHub")
+            .collection("bookings");
+
+        app.get("/categories", async (req, res) => {
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        // get all products
+
+        app.get("/products", async (req, res) => {
+            const query = {};
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        });
+        // get products by category
+
+        app.get("/products/:category", async (req, res) => {
+            const query = { category: req.params.category };
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+
+    } finally { }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
